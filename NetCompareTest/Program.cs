c08f72d54c1acd943f2f188ae425c6fffbca51ae
Daniel Van Houten <atomicwebdev@gmail.com>
2014-03-04 15:47:42 -0600

    initial commit

---
+++ NetCompareTest/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace NetCompareTest
+{
+    class Program
+    {
+
+        public class User
+        {
+            public string Name { get; set; }
+            public int Number { get; set; }
+            public Address Address { get; set; }
+
+            public List<Phone> Phones { get; set; } 
+        }
+
+
+        public class Address
+        {
+            public string Street { get; set; }
+            public string State { get; set; }
+            public string Country { get; set; }
+        }
+
+        public class Phone
+        {
+            public string  Type { get; set; }
+            public string Number { get; set; }
+        }
+
+        static IEnumerable<string> GetPropertInfos( object o, string parent = null )
+        {
+            Type t = o.GetType();
+            PropertyInfo[] props = t.GetProperties( BindingFlags.Public | BindingFlags.Instance );
+            foreach ( PropertyInfo prp in props )
+            {
+                if ( prp.PropertyType.Module.ScopeName != "CommonLanguageRuntimeLibrary" )
+                {
+                    // fix me: you have to pass parent + "." + t.Name instead of t.Name if parent != null
+                    foreach ( var info in GetPropertInfos( prp.GetValue( o ), t.Name ) )
+                        yield return info;
+                }
+                else
+                {
+                    var value = prp.GetValue( o );
+                    var stringValue = ( value != null ) ? value.ToString() : "";
+                    var info = t.Name + "." + prp.Name + ": " + stringValue;
+                    if ( String.IsNullOrWhiteSpace( parent ) )
+                        yield return info;
+                    else
+                        yield return parent + "." + info;
+                }
+            }
+        }
+
+
+        static void Main( string[] args )
+        {
+
+
+            var user = new User
+            {
+                Name = "Foo",
+                Number = 19,
+                Address = new Address
+                                {
+                                    Street = "MyStreet", 
+                                    State = "MyState", 
+                                    Country = "SomeCountry"
+                                },
+                Phones = new List<Phone>
+                            {
+                                new Phone{ Number = "334-558-2060", Type = "Home"},
+                                new Phone{ Number = "334-558-2061", Type = "Cell"}
+                            }
+            };
+
+            foreach ( var info in GetPropertInfos( user ) )
+                Console.WriteLine( info );
+
+            Console.Read();
+        }
+    }
+}
---
+++ NetCompareTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle( "NetCompareTest" )]
+[assembly: AssemblyDescription( "" )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "EBSCO Industries, Inc." )]
+[assembly: AssemblyProduct( "NetCompareTest" )]
+[assembly: AssemblyCopyright( "Copyright © EBSCO Industries, Inc. 2014" )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible( false )]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid( "1910eabc-400c-4619-bb8f-12c88c92afa4" )]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion( "1.0.0.0" )]
+[assembly: AssemblyFileVersion( "1.0.0.0" )]
